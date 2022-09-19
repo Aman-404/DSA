8de64b9757b524b7a2559e47bbdde7115c9ffcb9
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-09-20 02:53:17 +0530

    Time: 11 ms (96.78%), Space: 115.7 MB (84.60%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,9 +1,16 @@
 class Solution {
-    public int[] dailyTemperatures(int[] T) {
-        int[] result = new int[T.length];
-        for (int day = T.length - 1; day >= 0; day--)
-            for (int i = day - 1; i >= 0 && T[i] < T[day]; i--)  
-                result[i] = day - i;
+    public int[] dailyTemperatures(int[] arr) {
+        
+        int[] result = new int[arr.length];
+        
+        for (int day = arr.length - 1; day >= 0; day--){
+           for (int i = day - 1; i >= 0 && arr[i] < arr[day]; i--){
+               result[i] = day - i; 
+           } 
+                
+        }
+            
+                
         return result;
     }
 }
