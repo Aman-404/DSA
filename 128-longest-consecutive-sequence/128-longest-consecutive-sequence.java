89d9f8f3fa7f33fdf2a1870b86356f4d6194d8a0
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-09-21 17:55:21 +0530

    Time: 67 ms (57.90%), Space: 72.8 MB (66.47%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,26 +1,26 @@
 class Solution {
-      public int longestConsecutive(int[] nums) {
-        int res = 0;// answer len
-        Set<Integer> set = new HashSet<>();
-        for(int i:nums) set.add(i); // add all elements in a set, we dont require duplicates because - 
-        //suppose we have 1 1 2 2 3 3 in the array the max len will be 3 - (1 2 3) doesnt matter how many times a number is present 
-        for(int i:nums){
-            // the idea is to assume that the present value "i" is the center of the sequence in which it is present 
-            // then we will go left and right of it to find the length of its sequence
-            // suppose we have 5 1 2 3 4 6 7 8, now the first element we have is 5
-            int max = 1,prevVal = i-1,nextVal = i+1; // max is the current length as we have one elemet in the sequence that is 5
-            // now for 5 prevVal = 4
-            while(set.contains(prevVal)){ // if 4 is present in the set i.e. in the array
-                max++; // increase the sequence len
-                set.remove(prevVal--); // remove 4 and decremennt prevVal to 3 and continue 
-            }
-            // removing the elements because an element can only be part of only one consecutive sequence
-            while(set.contains(nextVal)){ // same for the nextVal of the sequence
-                max++;
-                set.remove(nextVal++);
-            }
-            res = Math.max(res,max); // maintaining max len 
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
         }
-        return res;
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
     }
 }
