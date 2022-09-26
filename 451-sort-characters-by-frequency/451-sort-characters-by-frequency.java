545fe97650be99a8b143fc69d0d54a1d1d26eed8
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-09-26 15:59:41 +0530

    Time: 41 ms (34.68%), Space: 49.2 MB (58.40%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,18 +1,19 @@
 class Solution {
-    public String frequencySort(String s) {
+   public String frequencySort(String s) {
 	// Count the occurence on each character
 	HashMap<Character, Integer> cnt = new HashMap<>();
-	for (char c : s.toCharArray()) {
+	for (char c: s.toCharArray()) {
 		cnt.put(c, cnt.getOrDefault(c, 0) + 1);
 	}
-	
-	// Sorting
-	List<Character> chars = new ArrayList(cnt.keySet());
-	Collections.sort(chars, (a, b) -> (cnt.get(b) - cnt.get(a)));
+
+	// Build heap
+	PriorityQueue<Character> heap = new PriorityQueue<>((a, b) -> (cnt.get(b) - cnt.get(a)));
+	heap.addAll(cnt.keySet());
 
 	// Build string
 	StringBuilder sb = new StringBuilder();
-	for (Object c : chars) {
+	while (!heap.isEmpty()) {
+		char c = heap.poll();
 		for (int i = 0; i < cnt.get(c); i++) {
 			sb.append(c);
 		}
