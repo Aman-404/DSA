f941fa9019dd0e716edf33be4f4977b18aaab5dd
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-09-28 18:00:59 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (92.42%) - LeetHub

---
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.java
@@ -0,0 +1,12 @@
+class Solution {
+   public boolean canBeIncreasing(int[] nums) {
+	for (int i = 0, faults = 0; i + 1 < nums.length; i++)
+		if (nums[i] >= nums[i + 1]) {
+			if (++faults == 2)
+				return false;
+			if (i != 0 && nums[i - 1] >= nums[i + 1])
+				nums[i + 1] = nums[i];
+		}
+	return true;
+}
+}
