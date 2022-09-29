a801cb17fbd62ac5755aff1dab152fb56a4a9c25
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-09-30 00:06:58 +0530

    Time: 3 ms (34.49%), Space: 42.6 MB (24.03%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,11 +1,13 @@
 class Solution {
-    public int numJewelsInStones(String je, String s) {
-        int count =0;
-        for(int i=0;i<je.length();i++){
-            for(int j=0;j<s.length();j++){
-                if(je.charAt(i)==s.charAt(j)) count++;
-            }
-        }
-        return count;
+    public int numJewelsInStones(String jewels, String stones) {
+        List<Character> list = new ArrayList<>();
+        int ans = 0;
+        for(int i = 0; i < jewels.length(); i++)
+            list.add(jewels.charAt(i));
+        
+        for(int i = 0; i < stones.length(); i++)
+            if(list.contains(stones.charAt(i)))ans++;
+        
+        return ans;
     }
 }
