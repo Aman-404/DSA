b24079892e30b6a3a2fe270cf9059e783e16b74f
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-10-08 10:36:31 +0530

    Time: 10 ms (85.19%), Space: 54.3 MB (67.45%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,4 +1,5 @@
 class Solution {
+    
      class Pair implements Comparable<Pair>{
         String name;
         int height;
@@ -7,15 +8,16 @@ class Solution {
             this.height = height;
         }
         public int compareTo(Pair o){
-            return this.height - o.height;
+            return o.height - this.height;
         }
     }
     public String[] sortPeople(String[] names, int[] heights) {
         String[] ans = new String[names.length];
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
         for(int i = 0; i<names.length; i++){
             pq.add(new Pair(names[i], heights[i]));
+            
         }
         
         int i = 0;
