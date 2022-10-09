cf7ca137458098fab5d4d1d6b8d4f5d1a0a8a059
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-10-09 14:59:30 +0530

    Time: 12 ms (48.46%), Space: 43.7 MB (84.25%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,52 +1,22 @@
-class PairComparator implements Comparator<Pair>{
-    public int compare(Pair p1, Pair p2) {
-        if (p1.getKey() > p2.getKey())
-            return 1;
-        else if (p1.getKey() < p2.getKey())
-            return -1;
-        
-        if (p1.getValue() < p2.getValue())
-            return 1;
-        else if (p1.getValue() > p2.getValue())
-            return -1;
-        return 0;
-    }
-}
-public class Pair {
-    private Integer key;
-    private Integer value;
-    Pair(int key, int value){
-        this.key = key;
-        this.value = value;
-    }
-    
-    int getKey(){
-        return this.key;
-    }
-    int getValue(){
-        return this.value;
-    }
-}
+
 class Solution {
     public int[] frequencySort(int[] nums) {
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        PriorityQueue<Pair> queue = new PriorityQueue<>(new PairComparator());
-        map.forEach((k, v) -> {
-            queue.add(new Pair(v, k));
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (a, b) -> {
+            return (map.get(a) == map.get(b))? b - a : map.get(a) - map.get(b);
         });
-        
+            
         int[] res = new int[nums.length];
-        int c = 0;
-        while(!queue.isEmpty()){
-            Pair pair = queue.poll();
-            for(int i=0; i<pair.getKey(); i++){
-                res[c++] = pair.getValue();
-            } 
+        int i = 0;
+        for (int num : list) {
+            for (int j = 0; j < map.get(num); j++) {
+                res[i++] = num;
+            }
         }
         return res;
     }
