eb1ec3148a228cc8b296a8f3399a402fbb983c37
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-10-16 10:18:32 +0530

    Time: 76 ms (47.30%), Space: 143.9 MB (71.71%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,25 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(nums == null || nums.length == 0) return new int[0];
-        int[] res = new int[nums.length - k + 1];
-        Deque<Integer> deque = new ArrayDeque<>();
-        for(int i = 0; i < k; i++){
-            while(!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]){
-                deque.pollLast();
+        Deque<Integer> q = new ArrayDeque<>();  // stores *indices*
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!q.isEmpty() && nums[q.getLast()] <= nums[i]) {
+                q.removeLast();
             }
-            deque.offerLast(i);
+            q.addLast(i);
+            // remove first element if it's outside the window
+            if (q.getFirst() == i - k) {
+                q.removeFirst();
+            }
+            // if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
+            if (i >= k - 1) {
+                res.add(nums[q.peek()]);
+            }
         }
-        res[0] = nums[deque.peekFirst()];
-        for(int i = k; i < nums.length; i++){
-            while(!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]){
-                deque.pollLast();
-            }
-            if(!deque.isEmpty() && deque.peekFirst() <= i - k){
-                deque.pollFirst();
-            }
-            deque.offerLast(i);
-            res[i - k + 1] = nums[deque.peekFirst()];
-        }
-        return res;
+        return res.stream().mapToInt(i->i).toArray();            
     }
 }
