0673099a8ef9da225b7840b7b09b6e961efac404
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-10-22 06:43:57 +0530

    Time: 14 ms (50.32%), Space: 48.3 MB (54.49%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,36 +1,39 @@
 class Solution {
-    public int[] asteroidCollision(int[] ast) {
-        Stack<Integer> st = new Stack<>();
-        int n=ast.length;
-       for(int i=0;i<n;i++)
-        {
-            if(ast[i]<=0)
-            {
-                while(!st.isEmpty() && st.peek ()> 0 && st.peek()<ast[i]*-1)
-                    st.pop();
+    public int[] asteroidCollision(int[] asteroids) {
+        
+        Stack<Integer> stack = new Stack<>();
+        for(int asteroid : asteroids){
+            
+            boolean canPush = true;
+            while(!stack.isEmpty() && (stack.peek()*asteroid)<0){
+            
+                if(stack.peek() < 0 && asteroid > 0) break;
+                int absAsteroid = Math.abs(asteroid);
+                int absTop = Math.abs(stack.peek());
 
-                if(!st.isEmpty() && st.peek()==ast[i]*-1)
-                    st.pop();
-
-                else if(!st.isEmpty() && st.peek() >= -1*ast[i])
-                    continue;
-                else
-                    st.push (ast[i]);
+                if(absAsteroid > absTop)
+                    stack.pop();
+            
+                else if(absAsteroid == absTop){
+                    stack.pop();
+                    canPush = false;
+                    break;
+                }
+      
+                else {
+                    canPush = false;
+                    break;
+                }
             }
-           else
-               st.push(ast[i]);
 
-
+            if(canPush) stack.push(asteroid);
         }
-    
-    n=st.size();
-    int arr[]=new int[n];
-    
-    for(int i=n-1;i>=0;i--)
-    arr[i]=st.pop();
-    return arr;
-	
-	
-	}
-    
+
+        int size = stack.size();
+        int[] result = new int[size];
+        for(int i=size-1; i>=0; i--)
+            result[i] = stack.pop();
+
+        return result;
+    }
 }
