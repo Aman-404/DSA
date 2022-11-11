1dfb25e24730e691ef37e743f879c918a63a7379
Aman Upadhyay <54461981+Aman-404@users.noreply.github.com>
2022-11-11 06:54:22 +0530

    Time: 1 ms (100.00%), Space: 68.4 MB (25.50%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        int curr = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            
-            if (i > curr) {
-                return false;
-            } 
-            curr = Math.max(curr, i + nums[i]);  
+        int last = nums.length - 1; // last index/ position to reach 
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i+nums[i] >= last) { // check if i+nums[i] >= last means we can reach to that index 
+			//with curr value and from current index and if yes then 
+			//change last to current index
+                last = i;
+            }
         }
-        return true;
+        return last <= 0; // if last <= 0  then that means we can reach to solution if we start from begining also return true else false
     }
 }
